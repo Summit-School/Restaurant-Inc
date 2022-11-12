c8a1260719a1b5eb3bfa845120b2a217d4a43626
Enow Divine <sirdivine16@gmail.com>
2022-11-12 07:49:09 +0100

    admin dashboard header

---
+++ src/components/account/profileIcon/Profile.jsx
@@ -0,0 +1,53 @@
+import "./Profile.css";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { GoSignOut } from "react-icons/go";
+import UpdatePassword from "../updatePassword/UpdatePassword";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+const Profile = () => {
+  const [changePassword, setChangePassword] = useState(false);
+  const handleLogout = (e) => {
+    e.preventDefault();
+
+    console.lgo("logout");
+  };
+
+  return (
+    <div className="user-profile-icon">
+      <NavDropdown
+        title={
+          <div className="pull-left">
+            <span>test@gmail.com</span>
+            <img
+              className="thumbnail-image"
+              src="/images/logo.jpg"
+              alt="user pic"
+            />
+          </div>
+        }
+        id="basic-nav-dropdown"
+      >
+        <NavDropdown.Item className="menu-item">
+          <Link to="#" onClick={() => setChangePassword(true)}>
+            <RiLockPasswordLine className="m-1" />
+            Change Password
+          </Link>
+        </NavDropdown.Item>
+        <NavDropdown.Item className="menu-item text-danger">
+          <Link to="#" className="signout-link" onClick={handleLogout}>
+            <GoSignOut className="m-1" />
+            Sign Out
+          </Link>
+        </NavDropdown.Item>
+      </NavDropdown>
+      <UpdatePassword
+        show={changePassword}
+        onHide={() => setChangePassword(false)}
+      />
+    </div>
+  );
+};
+
+export default Profile;
