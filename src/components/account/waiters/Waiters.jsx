04f5e386b5c7201f9995488cdb975f99f3778e9c
Enow Divine <sirdivine16@gmail.com>
2022-11-14 05:28:28 +0100

    staff age complete

---
+++ src/components/account/waiters/Waiters.jsx
@@ -0,0 +1,58 @@
+import "./Waiters.css";
+import { FaUserAlt } from "react-icons/fa";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { RiLockPasswordFill } from "react-icons/ri";
+
+const Waiters = () => {
+  return (
+    <div className="accordion" id="accordionExample">
+      <div className="pending-heading">Waiters</div>
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingOne">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            Mike Dane
+          </button>
+        </h2>
+        <div
+          id="collapseOne"
+          className="accordion-collapse collapse show"
+          aria-labelledby="headingOne"
+          data-bs-parent="#accordionExample"
+        >
+          <div className="accordion-body">
+            <div className="name">
+              <div className="data-key">
+                <FaUserAlt />
+                <span> Name:</span>
+              </div>
+              <div className="data-value">Mike Dane</div>
+            </div>
+            <div className="phone-number">
+              <div className="data-key">
+                <BsFillTelephoneFill />
+                <span>Phone Number:</span>
+              </div>
+              <div className="data-value">+237 672491296</div>
+            </div>
+            <div className="password">
+              <div className="data-key">
+                <RiLockPasswordFill />
+                <span>Password:</span>
+              </div>
+              <div className="data-value">1234MyRestaurant</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Waiters;
--- src/pages/account/users/Users.jsx
+++ src/pages/account/users/Users.jsx
@@ -1,15 +1,24 @@
 import "./Users.css";
 import Layout from "../../../components/layout/Layout";
+import Waiters from "../../../components/account/waiters/Waiters";
+import Cashiers from "../../../components/account/cashiers/Cashiers";
+import Kitchen from "../../../components/account/kitchen/Kitchen";
 
 const Users = () => {
   return (
     <Layout>
       <div className="container staff-page-wrapper">
         <div className="staff-page-scroller">
-          <div className="service">service</div>
+          <div className="service">
+            <Waiters />
+          </div>
           <div className="cash-and-kit">
-            <div className="cashier">cashier</div>
-            <div className="kitchen">kitchen</div>
+            <div className="cashier">
+              <Cashiers />
+            </div>
+            <div className="kitchen">
+              <Kitchen />
+            </div>
           </div>
         </div>
       </div>
