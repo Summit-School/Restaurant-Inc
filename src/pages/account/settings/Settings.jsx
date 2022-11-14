ba3e4379a5f1179759992bf5e69890f444947477
Enow Divine <sirdivine16@gmail.com>
2022-11-14 05:55:15 +0100

    settings page

--- src/pages/account/settings/Settings.jsx
+++ src/pages/account/settings/Settings.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import "./Settings.css";
 import Layout from "../../../components/layout/Layout";
-// import Settings from "../../../components/account/settings/Settings";
 
 const Settings = () => {
   return (
     <Layout>
-      <div className="settings">settings</div>
+      <div className="container settings-page-wrapper">
+        <div className="add-staff">
+          <p>Add Staff</p>
+          <form>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Staff name"
+            />
+            <input
+              className="form-control"
+              type="number"
+              placeholder="Phone number"
+            />
+            <input
+              className="form-control"
+              type="password"
+              placeholder="*********"
+            />
+            <button>Submit</button>
+          </form>
+        </div>
+        <div className="add-table">
+          <p>Add Table</p>
+          <form>
+            <input
+              className="form-control"
+              type="number"
+              placeholder="Table number"
+            />
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 };
