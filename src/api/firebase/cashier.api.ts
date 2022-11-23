f594ce759b7306793c3fd6b46b09ba414744ff38
oben-desmond <obend678@gmail.com>
2022-11-23 12:12:42 +0100

    mark as paid function updates

--- src/api/firebase/cashier.api.ts
+++ src/api/firebase/cashier.api.ts
@@ -22,11 +22,20 @@ import * as  uuid from "uuid";
 
 export async function markOrderAsPaid(order: Order, user: User) {
 
+    if (!order.id) {
+        const error = new Error();
+        error.message = " You need to provide an order with an id"
+        throw error;
+    }
+
     order.state = "PAID";
     order.cashier = user;
 
     const pendingOrderRef = doc(db, "all_tables", order.table.id);
+    const orderRef = doc(db, "all_others", order.id);
+    await setDoc(pendingOrderRef, { id: order.table.id, order });
+    await setDoc(orderRef, { id: order.id, order });
 
-    return setDoc(pendingOrderRef, { id: order.table.id, order });
+    return { message: "successfully paid for order" };
 }
 
