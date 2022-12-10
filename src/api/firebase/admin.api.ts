611684af20d48c04a4b0232fbceecad0590520a4
Enow Divine <sirdivine16@gmail.com>
2022-12-10 16:02:58 +0100

    getting array of orders

--- src/api/firebase/admin.api.ts
+++ src/api/firebase/admin.api.ts
@@ -22,7 +22,7 @@ import { sendNotification } from "../oneSignal/notifications.api.ts";
 import { loginAdmin } from "./auth.api.ts";
 import { updateInventory } from "./menu.api.ts";
 
-export function addToInventory(inventory: InventoryItem) { }
+export function addToInventory(inventory: InventoryItem) {}
 
 /**
  * Gets all orders that are currently pending
@@ -63,12 +63,12 @@ export function fetchAllOrders(callBack: (orders: Order[]) => void) {
 
 export async function AddOrderToPending(order: Order, user: User) {
   const tableOrders = await isTableOccupied(order.table.id);
-  if (tableOrders) {
-    const error = new Error();
-    error.message =
-      "Table is already occupied please free table before placing order";
-    throw error;
-  }
+  // if (tableOrders) {
+  //   const error = new Error();
+  //   error.message =
+  //     "Table is already occupied please free table before placing order";
+  //   throw error;
+  // }
   order.id = uuid.v4();
   order.state = "ORDERED";
   order.service = user;
@@ -85,9 +85,6 @@ export async function AddOrderToPending(order: Order, user: User) {
   return { message: "successfully placed order" };
 }
 
-
-
-
 export async function AddOrderToTable(order: Order, user: User) {
   const tableOrders = await isTableOccupied(order.table.id);
 
@@ -98,7 +95,10 @@ export async function AddOrderToTable(order: Order, user: User) {
 
   const tableOrderRef = doc(db, "all_tables", order.table.id);
 
-  await setDoc(tableOrderRef, { ...order.table, orders: [...(tableOrders || []), order] } as Table);
+  await setDoc(tableOrderRef, {
+    ...order.table,
+    orders: [...(tableOrders || []), order],
+  } as Table);
   await sendNotification({
     title: "placed order",
     description: `An order has just been added to table ${order.table.id}`,
@@ -106,9 +106,6 @@ export async function AddOrderToTable(order: Order, user: User) {
 
   return { message: "successfully placed order" };
 }
-
-
-
 
 /**
  * Get adds an order for a table
@@ -124,28 +121,29 @@ export async function serveOrder(order: Order, user: User) {
 
   if (!tableOrders) {
     const error = new Error();
-    error.message = "there are no orders on this table"
+    error.message = "there are no orders on this table";
     throw error;
   }
 
-  const matchedIndex = tableOrders.findIndex(thisOrder => {
+  const matchedIndex = tableOrders.findIndex((thisOrder) => {
     return thisOrder.id == order.id;
-  })
+  });
 
   if (matchedIndex >= 0) {
     tableOrders[matchedIndex] = order;
 
     const pendingOrderRef = doc(db, "all_tables", order.table.id);
 
-    await setDoc(pendingOrderRef, { ...order.table, orders: tableOrders } as Table);
+    await setDoc(pendingOrderRef, {
+      ...order.table,
+      orders: tableOrders,
+    } as Table);
     await updateInventory(order.drinks);
     await sendNotification({
       title: "served order",
       description: `Table ${order.table.id} has just been served`,
     });
   }
-
-
 
   return { message: "successfully served order" };
 }
@@ -157,7 +155,9 @@ export async function serveOrder(order: Order, user: User) {
  * @returns the order if the table is occupied or null otherwise
  */
 
-export async function isTableOccupied(tableId: string): Promise<Order[] | null> {
+export async function isTableOccupied(
+  tableId: string
+): Promise<Order[] | null> {
   const tableRef = doc(db, "all_tables", tableId);
   return getDoc(tableRef).then((res) => {
     const data = res.data() as Table;
--- src/components/service_account/pending_tables/PendingTables.jsx
+++ src/components/service_account/pending_tables/PendingTables.jsx
@@ -2,20 +2,36 @@ import { useState, useEffect } from "react";
 import { onSnapshotGetAllTables } from "../../../api/firebase/admin.api.ts";
 
 const PendingTables = () => {
+  const [sorted, setSorted] = useState([]);
+  const [allOrders, setAllOrders] = useState([]);
   const [pendingList, setPendingList] = useState([]);
 
   useEffect(() => {
     const cashier = JSON.parse(localStorage.getItem("cashier"));
     onSnapshotGetAllTables((response) => {
-      console.log(response);
-      console.log(cashier.phone);
-      let orders = response.filter(
-        (order) =>
-          order.order &&
-          order.order.service.phone === cashier.phone &&
-          (order.order.state === "ORDERED") | (order.order.state === "SERVED")
+      console.log("Tables", response);
+
+      // Get all tables with orders
+      let output = response.filter((output) => output.orders);
+      setSorted(output);
+
+      // Filter tables and return the orders array
+      let orders = [];
+      output.map((order) => orders.push(order.orders));
+      setAllOrders(orders);
+
+      // Filter orders according to state
+      // let finalOrders = [];
+      let finalOrders = orders.map((order) =>
+        order.map(
+          (orderObj) =>
+            orderObj.service.phone === cashier.phone &&
+            (orderObj.state === "ORDERED") | (orderObj.state === "SERVED")
+        )
       );
-      setPendingList(orders.reverse());
+
+      console.log("sorted", orders);
+      console.log("orders", finalOrders);
     });
   }, []);
 
@@ -28,101 +44,102 @@ const PendingTables = () => {
     <div className="container service-pending-tables">
       <div className="accordion" id="accordionExample">
         <div className="pending-heading">Pending Tables</div>
-        {pendingList.length > 0
-          ? pendingList.map((order, index) => {
-              let drinkTotal = 0;
-              let foodTotal = 0;
-              order.order.drinks.map((drink) => {
-                drinkTotal += drink.price * drink.quantity;
-              });
-              order.order.food.map((food) => {
-                foodTotal += food.price * food.quantity;
-              });
-              return (
-                <div className="accordion-item" key={index}>
-                  <h2 className="accordion-header" id="headingOne">
-                    <button
-                      className="accordion-button"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target={`#collapseOne${index}`}
-                      aria-expanded="true"
-                      aria-controls="collapseOne"
+        {allOrders.length > 0
+          ? allOrders.map((pendingList) =>
+              pendingList.map((order, index) => {
+                let drinkTotal = 0;
+                let foodTotal = 0;
+                order.drinks.map((drink) => {
+                  drinkTotal += drink.price * drink.quantity;
+                });
+                order.food.map((food) => {
+                  foodTotal += food.price * food.quantity;
+                });
+                return (
+                  <div className="accordion-item" key={index}>
+                    <h2 className="accordion-header" id="headingOne">
+                      <button
+                        className="accordion-button"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#collapseOne${index}`}
+                        aria-expanded="true"
+                        aria-controls="collapseOne"
+                      >
+                        Table number {order.table.id}
+                        {order.state === "ORDERED" ? (
+                          <span
+                            className="status"
+                            style={{ backgroundColor: "yellow", color: "grey" }}
+                          >
+                            {order.state}
+                          </span>
+                        ) : order.state === "SERVED" ? (
+                          <span
+                            className="status"
+                            style={{ backgroundColor: "blue", color: "white" }}
+                          >
+                            {order.state}
+                          </span>
+                        ) : (
+                          <span
+                            className="status"
+                            style={{ backgroundColor: "green", color: "white" }}
+                          >
+                            {order.state}
+                          </span>
+                        )}
+                      </button>
+                    </h2>
+                    <div
+                      id={`collapseOne${index}`}
+                      className="accordion-collapse collapse"
+                      aria-labelledby="headingOne"
+                      data-bs-parent="#accordionExample"
                     >
-                      Table number {order.order.table.id}
-                      {order.order.service.phone}
-                      {order.order.state === "ORDERED" ? (
-                        <span
-                          className="status"
-                          style={{ backgroundColor: "yellow", color: "grey" }}
-                        >
-                          {order.order.state}
-                        </span>
-                      ) : order.order.state === "SERVED" ? (
-                        <span
-                          className="status"
-                          style={{ backgroundColor: "blue", color: "white" }}
-                        >
-                          {order.order.state}
-                        </span>
-                      ) : (
-                        <span
-                          className="status"
-                          style={{ backgroundColor: "green", color: "white" }}
-                        >
-                          {order.order.state}
-                        </span>
-                      )}
-                    </button>
-                  </h2>
-                  <div
-                    id={`collapseOne${index}`}
-                    className="accordion-collapse collapse"
-                    aria-labelledby="headingOne"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div className="accordion-body">
-                      <ul>
-                        {order.order.food.map((item, index) => (
-                          <li key={index}>
-                            <span className="item">{item.itemName}</span>
-                            <span className="item">{item.quantity}</span>
-                            <span className="price">
-                              {formatMoney(item.price * item.quantity)} FCFA
+                      <div className="accordion-body">
+                        <ul>
+                          {order.food.map((item, index) => (
+                            <li key={index}>
+                              <span className="item">{item.itemName}</span>
+                              <span className="item">{item.quantity}</span>
+                              <span className="price">
+                                {formatMoney(item.price * item.quantity)} FCFA
+                              </span>
+                            </li>
+                          ))}
+
+                          <li className="mt-3 total-list">
+                            <span className="total">Total price</span>
+                            <span className="total-price">
+                              {formatMoney(foodTotal)} FCFA
                             </span>
                           </li>
-                        ))}
+                        </ul>
 
-                        <li className="mt-3 total-list">
-                          <span className="total">Total price</span>
-                          <span className="total-price">
-                            {formatMoney(foodTotal)} FCFA
-                          </span>
-                        </li>
-                      </ul>
-
-                      <ul>
-                        {order.order.drinks.map((item, index) => (
-                          <li key={index}>
-                            <span className="item">{item.itemName}</span>
-                            <span className="item">{item.quantity}</span>
-                            <span className="price">
-                              {formatMoney(item.price * item.quantity)} FCFA
+                        <ul>
+                          {order.drinks.map((item, index) => (
+                            <li key={index}>
+                              <span className="item">{item.itemName}</span>
+                              <span className="item">{item.quantity}</span>
+                              <span className="price">
+                                {formatMoney(item.price * item.quantity)} FCFA
+                              </span>
+                            </li>
+                          ))}
+                          <li className="mt-3 total-list">
+                            <span className="total">Total price</span>
+                            <span className="total-price">
+                              {formatMoney(drinkTotal)} FCFA
                             </span>
                           </li>
-                        ))}
-                        <li className="mt-3 total-list">
-                          <span className="total">Total price</span>
-                          <span className="total-price">
-                            {formatMoney(drinkTotal)} FCFA
-                          </span>
-                        </li>
-                      </ul>
+                        </ul>
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })
+                );
+              })
+            )
           : "No Pending Oders"}
       </div>
     </div>
